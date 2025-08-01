2fbcbdd57162d734a489e162fb6feb6961c88efe
SS7D <57664265+SS7D@users.noreply.github.com>
2025-08-01 22:14:11 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -86,7 +86,7 @@ $result = $stmt->get_result();
 <head>
   <meta charset="UTF-8">
   <title>管理 QSL 卡</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="bootstrap.min.css" rel="stylesheet" />
 </head>
 <body class="bg-light">
   <main class="container py-5" style="max-width:800px;">
