680494d24c132d938307da61e86c24b25a785070
SS7D <57664265+SS7D@users.noreply.github.com>
2025-08-01 22:14:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ function buildQuery($params = []) {
 <head>
   <meta charset="UTF-8" />
   <title>QSL 卡记录</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="bootstrap.min.css" rel="stylesheet" />
 </head>
 <body class="bg-light">
   <main class="container py-5" style="max-width:800px;">
