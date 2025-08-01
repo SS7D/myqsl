d4f284ffad78f5c4c1695905ccd5d359eb0ac78a
SS7D <57664265+SS7D@users.noreply.github.com>
2025-08-01 22:14:58 +0800

    Update register_email.php

--- register_email.php
+++ register_email.php
@@ -58,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
   <meta charset="UTF-8" />
   <title>登记呼号和邮箱 - MyQSL</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="bootstrap.min.css" rel="stylesheet" />
 </head>
 <body class="bg-light">
   <main class="container py-5" style="max-width:600px;">
